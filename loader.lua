f72905ccc6a6ebdfe38721babbedca4a0b30dcd7
A88 <61717705+not-A88@users.noreply.github.com>
2026-06-01 01:03:54 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 --[[
     Project.
-             ██╗ ██████╗ ██╗   ██╗
-             ██║██╔═══██╗╚██╗ ██╔╝
-             ██║██║   ██║ ╚████╔╝
-        ██   ██║██║   ██║  ╚██╔╝
-        ╚█████╔╝╚██████╔╝   ██║
-         ╚════╝  ╚═════╝    ╚═╝
+             __         ______     ______     ______    
+            /\ \       /\  __ \   /\  == \   /\  ___\   
+            \ \ \____  \ \ \/\ \  \ \  __<   \ \  __\   
+             \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\ 
+              \/_____/   \/_____/   \/_/ /_/   \/_____/ 
+                                            
 ]]--
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/not-A88/project-joy/main/int.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/not-A88/project-lore/main/int.lua'))()
